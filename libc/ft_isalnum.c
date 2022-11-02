6f2112d0954a6bb145d773ff2d8da2c29ecaa71f
Julien <73828002+LcntJulien@users.noreply.github.com>
2022-11-02 12:14:07 +0100

    isalnum and norm

--- libc/ft_isalnum.c
+++ libc/ft_isalnum.c
@@ -6,26 +6,15 @@
 /*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 10:56:43 by jlecorne          #+#    #+#             */
-/*   Updated: 2022/11/02 11:13:37 by jlecorne         ###   ########.fr       */
+/*   Updated: 2022/11/02 12:10:21 by jlecorne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
-int ft_isalnum(int  c)
+int	ft_isalnum(int c)
 {
-    if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z')
-        return(1);
-    else if (c >= 0 && c <= 9)
-        return(1);
-    return(0);
-}
-
-int main()
-{
-    if (ft_isalnum(' ') == 0)
-        printf("Tout va bene ma bella");
-    if (ft_isalnum(' ') == 1)
-        printf("Pas bien !!!");
-    return(0);
+	if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z')
+		return (1);
+	if (c >= 0 && c <= 9)
+		return (1);
+	return (0);
 }
--- libc/ft_isdigit.c
+++ libc/ft_isdigit.c
@@ -5,27 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/02 09:37:00 by jlecorne          #+#    #+#             */
-/*   Updated: 2022/11/02 09:47:18 by jlecorne         ###   ########.fr       */
+/*   Created: 2022/11/02 11:33:48 by jlecorne          #+#    #+#             */
+/*   Updated: 2022/11/02 12:08:36 by jlecorne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
-int ft_isdigit(int c)
+int	ft_isdigit(int c)
 {
-    if ((c >= 0 && c <= 9))
-    {
-        return(1);
-    }
-    return(0);    
-}
-
-int main(int argc, char const *argv[])
-{
-    if  (ft_isdigit('A') == 0)
-        printf("isnotdigit");
-    if  (ft_isdigit('A') != 0)
-        printf("isdigit");
-    return 0;
+	if ((c >= 0 && c <= 9))
+		return (1);
+	return (0);
 }
