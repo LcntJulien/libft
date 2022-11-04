6a306c0ed4b0124e1d5548991ba8456e6be5f3e1
Julio <73828002+LcntJulien@users.noreply.github.com>
2022-11-04 16:02:12 +0100

    Update ft_memmove.c

--- libc/ft_memmove.c
+++ libc/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 18:02:37 by jlecorne          #+#    #+#             */
-/*   Updated: 2022/11/03 18:04:06 by jlecorne         ###   ########.fr       */
+/*   Updated: 2022/11/04 16:01:59 by jlecorne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,27 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
+	size_t i;
+
+	i = 0;
+	if (!dst || !src)
+		return (dst);
+	if ((size_t)dst - (size_t)src < len)
+	{
+		i = len - 1;
+		while (i < len)
+		{
+			*(unsigned char *)(dst + i) = *(unsigned char *)(src + i);
+			i--;
+		}
+	}
+	else
+	{
+		while (i < len)
+		{
+			*(unsigned char *)(dst + i) = *(unsigned char *)(src + i);
+			i++;
+		}
+	}
+	return (dst);
 }
