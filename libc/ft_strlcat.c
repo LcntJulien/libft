effcddf876757e9097690008749e0e977aae9e7c
Julio <73828002+LcntJulien@users.noreply.github.com>
2022-11-08 17:30:49 +0100

    Update ft_strlcat.c

--- libc/ft_strlcat.c
+++ libc/ft_strlcat.c
@@ -6,20 +6,30 @@
 /*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 18:55:11 by jlecorne          #+#    #+#             */
-/*   Updated: 2022/11/04 19:15:31 by jlecorne         ###   ########.fr       */
+/*   Updated: 2022/11/08 17:30:41 by jlecorne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include <string.h>
 
 size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 {
-    size_t  i;
+	size_t i;
+	size_t j;
+	size_t dstlen;
+	size_t srclen;
 
-    i = 0;
-    if ()
-    {
-        /* code */
-    }
-    
+	j = 0;
+	i = 0;
+	dstlen = ft_strlen(dst);
+	srclen = ft_strlen(src);
+	if (dstsize <= dstlen)
+		return (srclen + dstsize);
+	while (dst[i] != '\0' && i < dstsize)
+		i++;
+	while (src[j] != '\0' && i < dstsize - 1)
+		dst[i++] = src[j++];
+	dest[i] = '\0';
+	return (srclen + dstlen);
 }
