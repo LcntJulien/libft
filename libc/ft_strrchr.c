b0b5ed6a7c4e566c2bec55c6cf3d54490a0d0433
Julio <73828002+LcntJulien@users.noreply.github.com>
2022-11-08 21:25:10 +0100

    Create ft_strrchr.c

---
+++ libc/ft_strrchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/08 21:02:48 by jlecorne          #+#    #+#             */
+/*   Updated: 2022/11/08 21:25:02 by jlecorne         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <string.h>
+
+char	*strrchr(const char *s, int c)
+{
+	int i;
+
+	i = ft_strlen(s);
+
+	while (i >= 0)
+	{
+		if (s[i] == (char)c)
+			return ((char *)(s + i));
+		i--;
+	}
+	return (NULL);
+}
