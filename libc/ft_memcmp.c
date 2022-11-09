f51f518f39a791273c480cdab44aa386bbe3dd8e
Julio <73828002+LcntJulien@users.noreply.github.com>
2022-11-09 15:51:05 +0100

    Create ft_memcmp.c

---
+++ libc/ft_memcmp.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/09 15:45:57 by jlecorne          #+#    #+#             */
+/*   Updated: 2022/11/09 15:48:23 by jlecorne         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (*(unsigned char *)(s1 + i) != *(unsigned char *)(s2 + i))
+			return (*(unsigned char *)(s1 + i) - *(unsigned char *)(s2 + i));
+		i++;
+	}
+	return (0);
+}
