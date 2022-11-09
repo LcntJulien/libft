33f6a6dee05aa17ee3423a9c354c61106d520484
Julio <73828002+LcntJulien@users.noreply.github.com>
2022-11-09 19:26:39 +0100

    Create ft_calloc.c

---
+++ ft_calloc.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/09 18:32:35 by jlecorne          #+#    #+#             */
+/*   Updated: 2022/11/09 19:26:12 by jlecorne         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	size_t i;
+	void *p;
+
+	i = size * count;
+	p = (void *)malloc(i);
+	if (!p)
+		return (NULL);
+	ft_bzero(p, count);
+	return (p);
+}
