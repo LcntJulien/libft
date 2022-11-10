f1f8623a46281cd360cea2bf0fb89a318620cef9
Julien <73828002+LcntJulien@users.noreply.github.com>
2022-11-10 16:08:26 +0100

    Create ft_split.c

---
+++ libc/ft_split.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/10 12:38:45 by jlecorne          #+#    #+#             */
+/*   Updated: 2022/11/10 16:07:25 by jlecorne         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	counter(const char *s, char c)
+{
+	int	i;
+	int	r;
+
+	i = 0;
+	r = 0;
+	if (s[0] != '\0' && s)
+		r++;
+	while (s[i])
+	{
+		if (s[i] == c && s[i + 1] != c && s[i + 1] != '\0')
+			r++;
+		i++;
+	}
+	return (r);
+}
+
+char	*bring(char	*s, char c)
+{
+	int	i;
+	char	*p;
+
+	i = 0;
+	while (s[i] && s[i] != c)
+		i++;
+	p = malloc(sizeof(char *) * (i + 1));
+	if (!p)
+		return (0);
+	i = 0;
+	while (s[i] && s[i] != c)
+	{
+		p[i] = s[i];
+		i++;
+	}
+	p[i] = '\0';
+	return (p);
+}
+
+char	**ft_split(const char *s, char c)
+{
+	int i;
+	int j;
+	int cnt;
+	char **r;
+
+	i = 0;
+	j = 0;
+	cnt = counter(s, c);
+	r = malloc(sizeof(char *) * (cnt + 1));
+	while (s[i])
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		if (s[i] != '\0')
+		{
+            r[j] = bring((s + i), c);
+			j++;
+		}
+		while (s[i] && s[i] == c)
+			i++;
+	}
+	r[j] = '\0';
+	return (r);
+}
