49bca06b58035d8a45574b0a8752681e500d60d6
Julio <73828002+LcntJulien@users.noreply.github.com>
2022-11-10 09:58:08 +0100

    Create ft_substr.c

---
+++ libc/ft_substr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/10 09:47:21 by jlecorne          #+#    #+#             */
+/*   Updated: 2022/11/10 09:58:04 by jlecorne         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <string.h>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t i;
+	char *p;
+
+	i = 0;
+	p = (char *)malloc(len + 1);
+    if (!p)
+        return (NULL);    
+	while (s && i < len)
+	{
+		*(char *)(p + i) = *(char *)(s + start);
+        start++;
+		i++;
+	}
+    return (p);
+}
