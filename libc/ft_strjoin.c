373efe7748c1b7ad83cbbc14e90003bab5124ce6
Julien <73828002+LcntJulien@users.noreply.github.com>
2022-11-10 11:38:53 +0100

    Update ft_strjoin.c

--- libc/ft_strjoin.c
+++ libc/ft_strjoin.c
@@ -5,23 +5,34 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/10 09:58:45 by jlecorne          #+#    #+#             */
-/*   Updated: 2022/11/10 10:11:51 by jlecorne         ###   ########.fr       */
+/*   Created: 2022/11/10 11:15:03 by jlecorne          #+#    #+#             */
+/*   Updated: 2022/11/10 11:38:48 by jlecorne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-    int i;
-    int j;
-    int len;
-    char    *p;
+	int i;
+	int j;
+	int len;
+	char *p;
 
-    i = 0;
-    j = 0;
-    len = (ft_strlen(s1) + ft_strlen(s2) + 1);
-    p = malloc(sizeof(char) * len);
-    if (!p)
-        return (0);
-    
+	i = 0;
+	j = 0;
+	len = (ft_strlen(s1) + ft_strlen(s2) + 1);
+	p = malloc(sizeof(char) * len);
+	if (!p)
+		return (0);
+	while (s1[i])
+	{
+		p[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		p[i + j] = s2[j];
+		j++;
+	}
+	p[i + j] = '\0';
+	return (p);
 }
