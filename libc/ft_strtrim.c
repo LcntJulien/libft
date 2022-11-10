c497bbd4048bbad2a36b310489265e15bb11fca9
Julien <73828002+LcntJulien@users.noreply.github.com>
2022-11-10 12:36:50 +0100

    Update ft_strtrim.c

--- libc/ft_strtrim.c
+++ libc/ft_strtrim.c
@@ -6,7 +6,33 @@
 /*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 11:44:49 by jlecorne          #+#    #+#             */
-/*   Updated: 2022/11/10 11:44:50 by jlecorne         ###   ########.fr       */
+/*   Updated: 2022/11/10 12:36:37 by jlecorne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+int	checkset(char c, const char *set)
+{
+	while (set)
+	{
+		if (c == set++)
+			return (1);
+		return (0);
+	}
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	int		i;
+	int		j;
+	char	*p;
+
+	i = 0;
+	j = ft_strlen(s1);
+	while (checkset(s1[i]))
+		i++;
+	while (checkset(s1[j]))
+		j--;
+	return (ft_substr(*s1, i, (j - i)));
+}
