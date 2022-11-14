b165922a4947ff6909220434038505f8b84e4b4a
Julien <73828002+LcntJulien@users.noreply.github.com>
2022-11-14 17:12:58 +0100

    try correct split start otherway

--- libc/ft_split.c
+++ libc/ft_split.c
@@ -6,11 +6,12 @@
 /*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 12:38:45 by jlecorne          #+#    #+#             */
-/*   Updated: 2022/11/12 12:55:38 by jlecorne         ###   ########.fr       */
+/*   Updated: 2022/11/14 16:56:55 by jlecorne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 int	counter(const char *s, char c)
 {
@@ -21,24 +22,24 @@ int	counter(const char *s, char c)
 	r = 0;
 	if (s[0] != '\0' && s)
 		r++;
-	while (s[i])
+	while (s[i++])
 	{
 		if (s[i] == c && s[i + 1] != c && s[i + 1] != '\0')
 			r++;
-		i++;
 	}
 	return (r);
 }
 
-char	*bring(char *s, char c)
+char	*bring(const char *s, char c)
 {
 	int		i;
 	char	*p;
 
 	i = 0;
+	p = "";
 	while (s[i] && s[i] != c)
 		i++;
-	p = malloc(sizeof(char *) * (i + 1));
+	p = malloc(sizeof(char) * (i + 1));
 	if (!p)
 		return (NULL);
 	i = 0;
@@ -65,30 +66,60 @@ char	**squarrefree(char **p)
 	return (NULL);
 }
 
+char	**whileloop(const char *s, char c, int cnt)
+{
+	int		i;
+	int		j;
+	char	**r;
+
+	i = 0;
+	j = 0;
+	r = malloc(sizeof(char *) * (cnt + 1));
+	if (!r)
+		return (0);
+	while (*s)
+	{
+		while (*s && *s == c)
+			s++;
+		if (*s && *s != c)
+		{
+			r[j] = bring(s, c);
+			if (!(r[j]))
+				return (squarrefree(r));
+			j++;
+		}
+		while (*s && *s != c)
+			s++;
+		s++;
+	}
+	r[j] = 0;
+	return (r);
+}
+
 char	**ft_split(const char *s, char c)
 {
 	int		i;
 	int		j;
 	int		cnt;
-	char	**r;
 
 	i = 0;
 	j = 0;
 	cnt = counter(s, c);
-	r = malloc(sizeof(char *) * (cnt + 1));
-	while (s[i])
+	return (whileloop(s, c, cnt));
+}
+
+int	main(void)
+{
+	char str[] = " ol ol";
+	char **tab;
+	int i;
+	char c = ' ';
+
+	tab = ft_split(str, c);
+	i = 0;
+	while (tab[i])
 	{
-		while (s[i] && s[i] == c)
-			i++;
-		if (s[i] != '\0')
-		{
-			r[j] = bring((char *)(s + i), c);
-			if (!(r[j]))
-				return (squarrefree(r));
-			j++;
-		}
-		while (s[i] && s[i] == c)
-			i++;
+		printf("%s\n", tab[i]);
+		i++;
 	}
-	return (r);
 }
