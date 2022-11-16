c53af5f27f9344c175f6b547d8673aa91a404b1e
Julien <73828002+LcntJulien@users.noreply.github.com>
2022-11-16 13:18:41 +0100

    modifs

--- libc/ft_putnbr_fd.c
+++ libc/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 18:58:42 by jlecorne          #+#    #+#             */
-/*   Updated: 2022/11/15 19:12:19 by jlecorne         ###   ########.fr       */
+/*   Updated: 2022/11/16 13:15:33 by jlecorne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,27 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	long i;
-	char c;
+	char	c;
 
-	i = n;
-	if (i == -2147483648)
+	if (n == -2147483648)
 		write(fd, "-2147483648", 11);
 	else
 	{
-		if (i < 0)
+		if (n < 0)
 		{
 			write(fd, "-", 1);
-			i = -i;
-			ft_putnbr_fd(i, fd);
+			n = -n;
+			ft_putnbr_fd(n, fd);
 		}
-		else if (i < 10)
+		else if (n < 10)
 		{
-			c = i + '0';
+			c = n + '0';
 			write(fd, &c, 1);
 		}
 		else
 		{
-			ft_putnbr_fd(i / 10, fd);
-			c = (i % 10) + '0';
+			ft_putnbr_fd(n / 10, fd);
+			c = (n % 10) + '0';
 			write(fd, &c, 1);
 		}
 	}
--- libc/ft_strmapi.c
+++ libc/ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 18:18:34 by jlecorne          #+#    #+#             */
-/*   Updated: 2022/11/15 18:24:35 by jlecorne         ###   ########.fr       */
+/*   Updated: 2022/11/16 13:18:20 by jlecorne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	char *r;
-	int i;
+	char	*r;
+	int		i;
 
 	i = 0;
 	r = ft_strdup(s);
-    if(!r)
+	if (!r)
 		return (NULL);
 	while (r[i++])
 		r[i] = (*f)(i, r[i]);
