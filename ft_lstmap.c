b570b70506ad348d32ef90271af7426694dc7ea2
Julien <73828002+LcntJulien@users.noreply.github.com>
2023-04-20 12:13:16 +0200

    Update ft_lstmap.c

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: jlecorne <jlecorne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 17:18:46 by jlecorne          #+#    #+#             */
-/*   Updated: 2023/04/20 11:11:30 by jlecorne         ###   ########.fr       */
+/*   Updated: 2023/04/20 12:08:30 by jlecorne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,16 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list *new_lst;
-	t_list *elem;
+	t_list	*new_lst;
+	t_list	*elem;
 
 	if (!lst)
 		return (0);
 	new_lst = 0;
 	while (lst)
 	{
-		if (!(elem = ft_lstnew(f(lst->content))))
+		elem = ft_lstnew(f(lst->content));
+		if (!elem)
 		{
 			ft_lstclear(&new_lst, del);
 			return (0);
